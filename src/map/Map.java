c92bbb596bfbc2555aeea8d89261ac3b01fe7e31
Zhu Yupeng <tawoli@icloud.com>
2019-12-24 05:31:36 +0800

    map

---
+++ src/map/Map.java
@@ -0,0 +1,17 @@
+package map;
+
+public interface Map<K, V> {
+    int getSize();
+
+    boolean isEmpty();
+
+    void add(K key, V value);
+
+    V remove(K key);
+
+    boolean contains(K key);
+
+    V get(K key);
+
+    void set(K key, V newValue);
+}
